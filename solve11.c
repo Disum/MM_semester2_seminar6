b742ecad1d82ac8554a7b9791df280d206d133b5
yakov <mirenkovye@yandex.ru>
2020-03-27 22:01:55 +0300

    Fixed bugs

--- solve11.c
+++ solve11.c
@@ -20,19 +20,17 @@ void solve11(double *a, int m, int n)
 	j_max = i_max%n;
 	i_max /= n;
 
-	if( i_max>0 )
-		for( b = a; b<(a + n); b++ )
-		{
-			buffer = b[i_max*n];
-			b[i_max*n] = b[0];
-			b[0] = buffer;
-		}
+	for( b = a; b<(a + n); b++ )
+	{
+		buffer = b[i_max*n];
+		b[i_max*n] = b[0];
+		b[0] = buffer;
+	}
 
-	if( j_max>0 )
-		for( b = a; b<(a + m*n); b += n )
-		{
-			buffer = b[j_max];
-			b[j_max] = b[0];
-			b[0] = buffer;
-		}
+	for( b = a; b<(a + m*n); b += n )
+	{
+		buffer = b[j_max];
+		b[j_max] = b[0];
+		b[0] = buffer;
+	}
 }
